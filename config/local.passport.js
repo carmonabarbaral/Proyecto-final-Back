aa5e2f40e031e581f19a07e85cc0fc629e597b1c
carmonabarbaral <barbaracarmona40@mail.com>
2023-09-12 09:40:49 -0300

    cambios en el userModels, creacion de /current

--- config/local.passport.js
+++ config/local.passport.js
@@ -14,7 +14,7 @@ const initializepassport = () => {
             try {
                 const user = await userModels.findOne({email: username})
                 if (user) {
-                    console.log('Ususario existente')
+                    console.log('Usuario existente')
                     return done(null, false)
                 }
 
@@ -59,7 +59,7 @@ const initializepassport = () => {
     passport.use('github', new GitHubStrategy({
         clientID:'Iv1.1b3f45da60ecb07a',
         clientSecret:'28700ac60ec6b3fea59baf979996b4b56e7a8cb4',
-        callbackURL:'http://localhost:3000/api/session/github-callback',
+        callbackURL:'http://localhost:3000/api/sessions/github-callback',
     }, async(accsessToken, refreshToken, profile, done) => {
         try {
 
@@ -68,7 +68,8 @@ const initializepassport = () => {
             const existingUser = await userModels.findOne({
                 $or: [
                     { username: profile._json.login },
-                    { email: profile.emails[0].value }
+                    {email: profile.emails && profile.emails[0] ? profile.emails[0].value : null},
+                
                 ]
             });
 
@@ -80,7 +81,7 @@ const initializepassport = () => {
             const newUser = await userModels.create({
                 username: profile._json.login,
                 name: profile._json.name,
-                email: profile.emails[0].value
+                email:  profile.emails[0]
             })
             console.log('Nuevo Usuario:', newUser);
             return done(null, { _id: newUser._id, user: newUser });
